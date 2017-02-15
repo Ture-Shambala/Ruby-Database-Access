0d57be44834ac597b8812e9ea9c3f65a3bf4a216
Ture-Shambala <tureshambala4@gmail.com>
2017-02-15 16:20:39 +0530

    Completed delete functionality

--- movies.rb
+++ movies.rb
@@ -1,48 +1,58 @@
 require 'pg'
 require 'pry'
 
-begin
-  con = PG.connect :dbname => 'ruby_sample', :user => 'postgres', :password => 'root'
-  # con.exec "Create table Movie(Id SERIAL PRIMARY KEY, Name VARCHAR(20), Year_Release INT)"
-  # # con.exec "CREATE SEQUENCE movies_id;"
-  # # con.exec "CREATE TABLE movie ( id integer NOT NULL DEFAULT nextval('movies_id') )"
-  # # con.exec "ALTER TABLE movie ALTER id SET DEFAULT NEXTVAL('movies_id')"
-  # con.exec "INSERT INTO Movies VALUES(1,'DDLJ',1995)"
-  # con.exec "INSERT INTO Movies VALUES(2,'K3G',2001)"
-  # con.exec "INSERT INTO Movies VALUES(3,'DHOOM2',2006)"
-  # con.exec "INSERT INTO Movies VALUES(4,'KRISH',2006)"
-  # con.exec "INSERT INTO Movies VALUES(5,'PK',2014)"
-  p "Select following digit"
-  p "1 : Show all movies"
-  p "2 : Add new movie"
-  p "3 : Exit"
-  digit = gets.chomp
 
-  case digit
-    when "1"
-      movies = con.exec "Select * from movies;"
-      p "All Movies List"
-      p "Id | Name | Year_Release"
-      movies.each do |movie|
-        p "#{movie['id']} | #{movie['name']} | #{movie['year_release']}"
-      end
-    when "2"
-      p "Please enter name of movie"
-      mv_name = gets.chomp
-      p "Please enter release year of movie"
-      year = gets.chomp
-      con.exec "INSERT INTO Movies (name, year_release) VALUES ('#{mv_name}',#{year.to_i})"
-      # p "New record is"
-      # new_record = con.exec "select * from movies ORDER BY id DESC LIMIT 1"
-      # p new_record
-    when "3"
-    else
-      p "Thank you"
-  end
-
-
-rescue PG::Error => e
-  puts e.message 
-ensure
-  con.close if con
+def select_choice
+  puts "=============Select your choice============="
+  p "1 : Show all movies records"
+  p "2 : Add new movie record"
+  p "3 : Remove particular record"
+  p "4 : Exit"
+  puts "============================================"
+  digit=gets.chomp
+  db_connect(digit)
+  # hash_store(choice)
 end
+
+def all_records
+  @con = PG.connect :dbname => 'ruby_sample', :user => 'postgres', :password => 'root'
+  movies = @con.exec "Select * from movies;"
+  p "All Movies List"
+  p "Id | Name | Year_Release"
+  movies.each do |movie|
+    p "#{movie['id']} | #{movie['name']} | #{movie['year_release']}"
+  end
+end
+
+def db_connect(digit)
+  begin
+    case digit
+      when "1"
+        all_records
+        select_choice
+      when "2"
+        p "Please enter name of movie"
+        mv_name = gets.chomp
+        p "Please enter release year of movie"
+        year = gets.chomp
+        @con.exec "INSERT INTO Movies (name, year_release) VALUES ('#{mv_name}',#{year.to_i})"
+        p "New record added successfully"
+        all_records
+        select_choice
+      when "3"
+        p "Please enter either name nor year_release of particular movie that you want's to remove"
+        remove_record = gets.chomp
+        @con.exec "delete from movies where name = '#{remove_record}' OR year_release = #{remove_record}"
+        p "Remove single record successfully"
+        all_records
+        select_choice
+      else
+        p "Thank you"
+    end
+  rescue PG::Error => e
+    puts e.message 
+  # ensure
+  #   @con.close if @con
+  end
+end
+select_choice
